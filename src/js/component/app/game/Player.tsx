7ad4491f3577b87ea9ea023cf382001a6662451f
C. M. de Picciotto <d3p1@d3p1.dev>
2025-09-15 02:13:45 +0000

    feat: add physics to player [#1]

--- src/js/component/app/game/Player.tsx
+++ src/js/component/app/game/Player.tsx
@@ -2,15 +2,24 @@
  * @description Player
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {RigidBody} from '@react-three/rapier'
 import {config} from '../../../etc/config.ts'
 
 export const Player = () => {
   return (
-    <mesh position={[0, config.player.height * 0.5, 0]} castShadow={true}>
-      <icosahedronGeometry
-        args={[config.player.height * 0.5, config.player.details]}
-      />
-      <meshStandardMaterial color="mediumpurple" flatShading={true} />
-    </mesh>
+    <RigidBody
+      type="dynamic"
+      restitution={0.2}
+      friction={1}
+      position={[0, config.player.height * 0.5, 0]}
+      colliders="hull"
+    >
+      <mesh castShadow={true}>
+        <icosahedronGeometry
+          args={[config.player.height * 0.5, config.player.details]}
+        />
+        <meshStandardMaterial color="mediumpurple" flatShading={true} />
+      </mesh>
+    </RigidBody>
   )
 }
