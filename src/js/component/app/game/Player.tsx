c7370e571646436feb1b108f7be34c596ad8c6ce
C. M. de Picciotto <d3p1@d3p1.dev>
2025-09-20 02:11:05 +0000

    feat: add player controller [#1]

--- src/js/component/app/game/Player.tsx
+++ src/js/component/app/game/Player.tsx
@@ -3,17 +3,40 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {useKeyboardControls} from '@react-three/drei'
-import {RigidBody} from '@react-three/rapier'
-import {useEffect} from 'react'
+import {type RapierRigidBody, RigidBody, vec3} from '@react-three/rapier'
+import {useEffect, useRef} from 'react'
 import {config} from '../../../etc/config.ts'
 import {CONTROL} from '../../../types'
 
+const FORCE_IMPULSE = 0.5
+const TORQUE_IMPULSE = 0.05
+const JUMP_IMPULSE = FORCE_IMPULSE * 5
+
 export const Player = () => {
+  const player = useRef<RapierRigidBody>(null!)
   const [sub] = useKeyboardControls<(typeof CONTROL)[keyof typeof CONTROL]>()
 
   useEffect(() => {
     const unsubControls = sub((pressed) => {
-      console.log(pressed)
+      const torqueImpulse = {x: 0, y: 0, z: 0}
+      const forceImpulse = {x: 0, y: 0, z: 0}
+
+      if (pressed.forward || pressed.backward) {
+        forceImpulse.z += FORCE_IMPULSE * (-1) ** Number(pressed.forward)
+        torqueImpulse.x += TORQUE_IMPULSE * (-1) ** Number(pressed.forward)
+      }
+
+      if (pressed.leftward || pressed.rightward) {
+        forceImpulse.x += FORCE_IMPULSE * (-1) ** Number(pressed.leftward)
+        torqueImpulse.z += TORQUE_IMPULSE * (-1) ** Number(pressed.rightward)
+      }
+
+      if (pressed.jump) {
+        forceImpulse.y += JUMP_IMPULSE
+      }
+
+      player.current.applyImpulse(vec3(forceImpulse), true)
+      player.current.applyTorqueImpulse(vec3(torqueImpulse), true)
     })
 
     return () => {
@@ -28,6 +51,7 @@ export const Player = () => {
       friction={1}
       position={[0, config.player.height * 0.5, 0]}
       colliders="ball"
+      ref={player}
     >
       <mesh castShadow={true}>
         <icosahedronGeometry
