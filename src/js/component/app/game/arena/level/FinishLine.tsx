6c1b64c1d69efe9187e50cbee87e54ba755a5ebe
C. M. de Picciotto <d3p1@d3p1.dev>
2025-10-05 01:20:03 +0000

    feat: add has collide flag for finish line component [#1]

--- src/js/component/app/game/arena/level/FinishLine.tsx
+++ src/js/component/app/game/arena/level/FinishLine.tsx
@@ -3,17 +3,28 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {RigidBody} from '@react-three/rapier'
+import {useRef} from 'react'
 import {type Level as LevelType} from '../../../../../types/index.ts'
 import {Level} from '../Level.tsx'
 
 export const FinishLine: LevelType = ({position}) => {
+  const hasCollideRef = useRef(false)
+
+  const handleCollisionEnter = () => {
+    if (hasCollideRef.current) {
+      return
+    }
+
+    hasCollideRef.current = true
+  }
+
   return (
     <RigidBody
       type="fixed"
       position={position}
       restitution={0.2}
       friction={1}
-      onCollisionEnter={() => console.log('HELO')}
+      onCollisionEnter={handleCollisionEnter}
     >
       <Level position={[0, 0, 0]} />
     </RigidBody>
