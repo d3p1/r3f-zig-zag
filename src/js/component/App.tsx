22e8bcee4fdf786a31d3ec164b541e98b447a5e4
C. M. de Picciotto <d3p1@d3p1.dev>
2025-10-08 01:54:20 +0000

    feat: add finish menu [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -7,11 +7,14 @@ import {KeyboardControls} from '@react-three/drei'
 import {Physics} from '@react-three/rapier'
 import {Perf} from 'r3f-perf'
 import {config} from '../etc/config.ts'
+import {useStageStore} from '../store/stage.ts'
 import {Light} from './app/env/Light.tsx'
 import {Game} from './app/Game.tsx'
 import {CONTROL} from '../types'
 
 export const App = () => {
+  const isFinished = useStageStore((state) => state.isFinished)
+
   return (
     <KeyboardControls
       map={[
@@ -39,6 +42,10 @@ export const App = () => {
           <Game />
         </Physics>
       </Canvas>
+
+      {isFinished && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.6)] text-primary backdrop-blur-md"></div>
+      )}
     </KeyboardControls>
   )
 }
