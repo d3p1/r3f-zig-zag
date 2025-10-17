5145172d8ec09ac89c986dbfd1b2c18f0a2a31d1
C. M. de Picciotto <d3p1@d3p1.dev>
2025-10-17 23:53:09 +0000

    feat: remove hamburguer prize [#1]

--- src/js/component/app/game/arena/level/FinishLine.tsx
+++ src/js/component/app/game/arena/level/FinishLine.tsx
@@ -2,7 +2,7 @@
  * @description Finish line
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {Float, Text, useGLTF} from '@react-three/drei'
+import {Float, Text} from '@react-three/drei'
 import {RigidBody} from '@react-three/rapier'
 import {useRef} from 'react'
 import {useStageStore} from '../../../../../store/stage.ts'
@@ -11,9 +11,6 @@ import {Level} from '../Level.tsx'
 import {config} from '../../../../../etc/config.ts'
 
 export const FinishLine: LevelType = ({position}) => {
-  const hamburger = useGLTF(
-    'https://d3p1.github.io/three.js-journey/media/models/Hamburger/hamburger-draco.glb',
-  )
   const hasCollideRef = useRef(false)
   const finish = useStageStore((state) => state.finish)
 
@@ -38,16 +35,6 @@ export const FinishLine: LevelType = ({position}) => {
         <Text fontSize={1} position={[0, config.wall.height, 0]}>
           Finish
         </Text>
-
-        <primitive
-          object={hamburger.scene}
-          position={[0, 0, 0]}
-          scale={[
-            config.wall.height * 0.1,
-            config.wall.height * 0.1,
-            config.wall.height * 0.1,
-          ]}
-        />
       </Float>
 
       <Level position={[0, 0, 0]} />
